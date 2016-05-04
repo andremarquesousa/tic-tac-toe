9110f93158557ab7b16ca7e02bf4be5cb57358a0
Andre marques <andremarquesweb@gmail.com>
2016-05-04 14:58:02 -0300

    verify win

--- functions.js
+++ functions.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
         linesNumber = 3,
         columnsNumber = 3,
         positions = [],
-        player1 = true;
+        player = 'circle';
 
     canvas.width = width;
     canvas.height = height;
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function () {
             ctx.stroke();
             item.selected = true;
         },
-        simbolX: function(item) {
+        symbolX: function(item) {
             var padding = 30,
                 item = item;
 
@@ -84,18 +84,28 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    var clearCanvas = function(){
+        positions = [],
+        player = 'circle';
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        createSquares();
+        draw.lines();
+    }
+
     var verifyItem = function(x, y){
         for (var i = 0; i < positions.length; i++) {
                 var item = positions[i];
 
                 if ((x >= item.left && x <= item.right) && (y >= item.top && y <=item.bottom)) {
                     if (item.selected == false) {
-                        if (player1) {
+                        if (player == 'circle') {
                             draw.circle(item);
-                            player1 = false;
+                            item.type = player;
+                            player = 'symbolX';
                         } else {
-                            draw.simbolX(item);
-                            player1 = true;
+                            draw.symbolX(item);
+                            item.type = player;
+                            player = 'circle';
                         }
                     } else {
                     alert('já marcado');
@@ -104,9 +114,87 @@ document.addEventListener('DOMContentLoaded', function () {
         };
     }
 
+
+    /*GAME LOGIC*/
+    var verifyWin = function() {
+        if (positions[0].selected && positions[1].selected && positions[2].selected) {
+            if (positions[0].type == 'circle' && positions[1].type == 'circle' && positions[2].type == 'circle') {
+                alert('GANHOU');
+                clearCanvas();
+            } else if (positions[0].type == 'symbolX' && positions[1].type == 'symbolX' && positions[2].type == 'symbolX') {
+                alert('GANHOU');
+                clearCanvas();
+            }
+        } 
+        if (positions[3].selected && positions[4].selected && positions[5].selected) {
+            if (positions[3].type == 'circle' && positions[4].type == 'circle' && positions[5].type == 'circle') {
+                alert('GANHOU');
+                clearCanvas();
+            } else if (positions[3].type == 'symbolX' && positions[4].type == 'symbolX' && positions[5].type == 'symbolX') {
+                alert('GANHOU');
+                clearCanvas();
+            }
+        }
+        if (positions[6].selected && positions[7].selected && positions[8].selected) {
+            if (positions[6].type == 'circle' && positions[7].type == 'circle' && positions[8].type == 'circle') {
+                alert('GANHOU');
+                clearCanvas();
+            } else if (positions[6].type == 'symbolX' && positions[7].type == 'symbolX' && positions[8].type == 'symbolX') {
+                alert('GANHOU');
+                clearCanvas();
+            }
+        }
+        if (positions[0].selected && positions[3].selected && positions[6].selected) {
+            if (positions[0].type == 'circle' && positions[3].type == 'circle' && positions[6].type == 'circle') {
+                alert('GANHOU');
+                clearCanvas();
+            } else if (positions[0].type == 'symbolX' && positions[3].type == 'symbolX' && positions[6].type == 'symbolX') {
+                alert('GANHOU');
+                clearCanvas();
+            }
+        }
+        if (positions[1].selected && positions[4].selected && positions[7].selected) {
+            if (positions[1].type == 'circle' && positions[4].type == 'circle' && positions[7].type == 'circle') {
+                alert('GANHOU');
+                clearCanvas();
+            } else if (positions[1].type == 'symbolX' && positions[4].type == 'symbolX' && positions[7].type == 'symbolX') {
+                alert('GANHOU');
+                clearCanvas();
+            }
+        }
+        if (positions[2].selected && positions[5].selected && positions[8].selected) {
+            if (positions[2].type == 'circle' && positions[5].type == 'circle' && positions[8].type == 'circle') {
+                alert('GANHOU');
+                clearCanvas();
+            } else if (positions[2].type == 'symbolX' && positions[5].type == 'symbolX' && positions[8].type == 'symbolX') {
+                alert('GANHOU');
+                clearCanvas();
+            }
+        }
+        if (positions[0].selected && positions[4].selected && positions[8].selected) {
+            if (positions[0].type == 'circle' && positions[4].type == 'circle' && positions[8].type == 'circle') {
+                alert('GANHOU');
+                clearCanvas();
+            } else if (positions[0].type == 'symbolX' && positions[4].type == 'symbolX' && positions[8].type == 'symbolX') {
+                alert('GANHOU');
+                clearCanvas();
+            }
+        }
+        if (positions[2].selected && positions[4].selected && positions[6].selected) {
+            if (positions[2].type == 'circle' && positions[4].type == 'circle' && positions[6].type == 'circle') {
+                alert('GANHOU');
+                clearCanvas();
+            } else if (positions[2].type == 'symbolX' && positions[4].type == 'symbolX' && positions[6].type == 'symbolX') {
+                alert('GANHOU');
+                clearCanvas();
+            }
+        }
+    }
+
     /*EVENTS*/
     canvas.addEventListener('click', function(e){
         verifyItem(e.offsetX, e.offsetY);
+        verifyWin();
     })
 
     createSquares();
